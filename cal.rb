224f48a0725c08e0401d104558cbcc32226c1460
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-15 23:09:49 +0900

    ARGVのgetoptsメソッドを使えって簡潔にオプションからの引数を受け取るよう変更する

--- cal.rb
+++ cal.rb
@@ -1,25 +1,18 @@
 require 'optparse'
 require 'date'
 opt = OptionParser.new
-params = {}
-opt.on('-m') {|m| params[:m] = m }
-opt.on('-y') {|y| params[:y] = y }
+params = ARGV.getopts('m:y:')
 
-opt.parse!(ARGV)
+month = (params["m"] || Date.today.month).to_i
+year = (params["y"] || Date.today.year).to_i
 
-month_of_argv = ARGV[0].to_i
-year_of_argv = ARGV[1].to_i
-
-if month_of_argv < 1 || month_of_argv > 12
+if month < 1 || month > 12
   raise ArgumentError, "月は1-12の範囲を指定して下さい"
 end
 
-if year_of_argv < 1970 || year_of_argv > 2100
+if year < 1970 || year > 2100
   raise ArgumentError, "年は1970-2100の範囲を指定して下さい"
 end
-
-month = params[:m] ? month_of_argv : Date.today.month
-year = params[:y] ? year_of_argv : Date.today.year
 
 month_first_date = Date.new(year, month, 1)
 month_last_date = Date.new(year, month, -1)
