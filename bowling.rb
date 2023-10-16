a376afbb51460dbf5af35bfe7c6bb40215365e5f
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-16 12:24:57 +0900

    微修正

--- bowling.rb
+++ bowling.rb
@@ -11,16 +11,15 @@ calculable_scores = scores.map do |score|
 end.flatten.map(&:to_i)
 
 framed_scores = calculable_scores.each_slice(2).to_a
-p framed_scores
 
 total_score = 0
 framed_scores.each_with_index do |frame, index|
   total_score += frame.sum
 
-  if index < 9 # 最終フレームだけは最初のみ計算
+  if index < 9 # 最終フレームだけは最初のみ計算するので除外する
     if frame[0] == 10 # ストライクの場合
       total_score += framed_scores[index + 1].sum # 次のフレームの合計を足す
-      total_score += framed_scores[index + 2][0] if framed_scores[index + 1][0] == 10 # 次のスコアがストライクの場合、次のスコアの最初の値のみを入れる
+      total_score += framed_scores[index + 2][0] if framed_scores[index + 1][0] == 10 # 次のスコアがストライクの場合、次の次のスコアの最初の値のみを入れる
     elsif frame.sum == 10 # スペアの場合
       total_score += framed_scores[index + 1][0]
     end
