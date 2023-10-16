0fe3c668463694e975b09567beae5ca6767df195
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-16 12:22:31 +0900

    処理の見直しとコメントの付与

--- bowling.rb
+++ bowling.rb
@@ -1,5 +1,5 @@
 scores = ARGV[0].split(",")
-# p scores
+
 calculable_scores = scores.map{ |score|
   if score == "X"
     [10,0]
@@ -7,7 +7,6 @@ calculable_scores = scores.map{ |score|
     score
   end
 }.flatten.map(&:to_i)
-# p calculable_scores
 
 framed_scores = calculable_scores.each_slice(2).to_a
 p framed_scores
@@ -15,13 +14,13 @@ p framed_scores
 total_score = 0
 framed_scores.each_with_index { |frame, index|
   total_score += frame.sum
-  # binding.irb
-  if index < 9
-    if frame[0] == 10
-      total_score += framed_scores[index + 1].sum
-      total_score += framed_scores[index + 2].first if framed_scores[index + 1].first == 10 # 次のスコアがストライクの場合、次のスコアの最初の値のみを入れる
-    elsif frame.sum == 10
-      total_score += framed_scores[index + 1].first
+
+  if index < 9 # 最終フレームだけは最初のみ計算
+    if frame[0] == 10 # ストライクの場合
+      total_score += framed_scores[index + 1].sum # 次のフレームの合計を足す
+      total_score += framed_scores[index + 2][0] if framed_scores[index + 1][0] == 10 # 次のスコアがストライクの場合、次のスコアの最初の値のみを入れる
+    elsif frame.sum == 10 # スペアの場合
+      total_score += framed_scores[index + 1][0]
     end
   end
 }
