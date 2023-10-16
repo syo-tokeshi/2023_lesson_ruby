9b508c69988e7af7db0693f7974221a8d587f043
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-16 11:58:40 +0900

    each_with_indexを使った実装を完了

--- bowling.rb
+++ bowling.rb
@@ -1,4 +1,3 @@
-# binding.irb
 scores = ARGV[0].split(",")
 # p scores
 calculable_scores = scores.map{ |score|
@@ -14,13 +13,13 @@ framed_scores = calculable_scores.each_slice(2).to_a
 p framed_scores
 
 total_score = 0
-framed_scores.each.with_index(1) { |frame, index|
-  # binding.irb
+framed_scores.each_with_index { |frame, index|
   total_score += frame.sum
+  # binding.irb
   if index < 9
     if frame[0] == 10
       total_score += framed_scores[index + 1].sum
-      total_score += 10 if framed_scores[index + 1] == 10
+      total_score += framed_scores[index + 2].first if framed_scores[index + 1].first == 10 # 次のスコアがストライクの場合、次のスコアの最初の値のみを入れる
     elsif frame.sum == 10
       total_score += framed_scores[index + 1].first
     end
