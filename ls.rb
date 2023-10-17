47a21273194cb15e971172aa5700609f0cf9ab45
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-17 14:05:14 +0900

    表示数を切り替えられないバグを修正　+ 命名を大幅に変更する

--- ls.rb
+++ ls.rb
@@ -2,17 +2,19 @@
 
 require 'debug'
 file_list = Dir.glob('*')
+file_count = file_list.count
 max_filename_length = file_list.max_by(&:length).length
-whitespace = 5
-group_count = 3
+whitespace = 3
+displayed_column_count = 2
+column_count = (file_count / displayed_column_count.to_f).ceil
 
-grouped_files = file_list.each_slice(group_count).to_a
-shortage_file_count = group_count - grouped_files[-1].count # グループ化する際に余りが出たら個数を数える
-grouped_files[-1].push(*Array.new(shortage_file_count, '')) # 後にtransposeでグループの個数を合わせるために、穴埋めファイルを作る
-displayed_file_group = grouped_files.transpose
+displayed_file_list = file_list.each_slice(column_count).to_a
+shortage_file_count = column_count - displayed_file_list[-1].count # グループ化する際に余りが出たら個数を数える
+displayed_file_list[-1].push(*Array.new(shortage_file_count, '')) # 後にtransposeでグループの個数を合わせるために、穴埋めファイルを作る
+transposed_file_list = displayed_file_list.transpose
 
-displayed_file_group.each do |group|
-  puts group.map { |file|
+transposed_file_list.each do |files|
+  puts files.map { |file|
     file.ljust(max_filename_length + whitespace, ' ')
   }.join
 end
