39452930d04d5978dce91fea9f10e8d728937246
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-17 17:47:22 +0900

    引数にファイルのパスを指定できるように変更

---
+++ ls2.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'debug'
+
+file_list = ARGV[0] ? Dir.glob('*', base: ARGV[0]) : Dir.glob('*') # baseを指定すると、こちらを基準にファイルを探す
+file_count = file_list.count
+max_filename_length = file_list.max_by(&:length).length
+whitespace = 3
+displayed_column_count = 2
+column_count = (file_count / displayed_column_count.to_f).ceil # ceilで切り上げるためには、to_fで小数点を付ける必要がある
+
+displayed_file_list = file_list.each_slice(column_count).to_a
+shortage_file_count = column_count - displayed_file_list[-1].count # グループ化する際に余りが出たら個数を数える
+displayed_file_list[-1].push(*Array.new(shortage_file_count, '')) # 後にtransposeでグループの個数を合わせるために、穴埋めファイルを作る
+transposed_file_list = displayed_file_list.transpose
+
+transposed_file_list.each do |files|
+  puts files.map { |file|
+    file.ljust(max_filename_length + whitespace, ' ')
+  }.join
+end
