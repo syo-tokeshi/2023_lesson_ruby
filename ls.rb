8e024d07bd51439ac8ce289e63f8f718ff938916
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-10-17 13:29:14 +0900

    lsコマンドのように表示される最初のバージョン

---
+++ ls.rb
@@ -0,0 +1,16 @@
+require 'debug'
+file_list = Dir.glob("*")
+max_filename_length = file_list.max_by { |file| file.length}.length
+whitespace = 5
+group_count = 3
+
+grouped_files = file_list.each_slice(group_count).to_a
+shortage_file_count = group_count - grouped_files[-1].count # グループ化する際に余りが出たら個数を数える
+grouped_files[-1].push(*(Array.new(shortage_file_count,""))) # 後にtransposeでグループの個数を合わせるために、穴埋めファイルを作る
+displayed_file_group = grouped_files.transpose
+
+displayed_file_group.each { |group|
+  p group.map{ |file|
+    file.ljust(max_filename_length + whitespace, " ")
+  }.join
+}
