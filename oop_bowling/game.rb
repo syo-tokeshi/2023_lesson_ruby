d93a2ffbd08750b5639a505839f029d136ed52b3
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-11-06 07:33:25 +0900

    gameクラスがscoreとフレーム情報を持っていて、フレームの合計を算出できるようにする

--- oop_bowling/game.rb
+++ oop_bowling/game.rb
@@ -1,10 +1,16 @@
 require 'frame'
+require 'score'
 class Game
-  def initialize(scores)
-    @frames = Frame.new(scores) # scoresの中身は何かあまり気にせず、newすると良い感じに変換されているイメージを持つ
+  def initialize(score)
+    @score = Score.new(score) # scoreの中身は何かあまり気にせず、newすると良い感じに変換されているイメージを持つ
+    @frames = []
   end
 
   def total_scores
+    scores = @score.convert_score_to_scores
+    scores.each_slice(2) do |s|
+      @frames += Frame.new(s)
+    end
     @frames.sum # gameが保有しているブウリングの各フレームを全部足すイメージ
   end
 end
