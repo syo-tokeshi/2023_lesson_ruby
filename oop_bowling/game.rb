2c3be1be27d25365a36fcf0c3275d9543ea0c07a
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-11-06 08:12:33 +0900

    gameクラスをリファクタし、合計値を算出できるよう修正

--- oop_bowling/game.rb
+++ oop_bowling/game.rb
@@ -4,12 +4,25 @@ class Game
   def initialize(score)
     @scores = Score.new(score) # scoreの中身は何かあまり気にせず、newすると良い感じに変換されているイメージを持つ
     @frames = []
-  end
-
-  def total_scores
     @scores.each_slice(2) do |s|
       @frames += Frame.new(s) # ボウリングのフレームは１投目&2投目のペアなのは明確なので、この渡し方で良さそう
     end
-    @frames.sum # gameが保有しているブウリングの各フレームを全部足すイメージ
+  end
+
+  def total_scores
+    total_point = 0
+    @frames.each_with_index do |frame, index|
+      total_point += frame.total_scores
+
+      if index < 9 # 最終フレームだけは最初のみ計算するので除外する
+        if frame[0] == 10 # ストライクの場合
+          total_point += @frames[index + 1].total_scores # 次のフレームの合計を足す
+          total_point += @frames[index + 2][0] if @frames[index + 1][0] == 10
+        elsif frame.total_scores == 10 # スペアの場合
+          total_point += @frames[index + 1][0]
+        end
+      end
+    end
+    total_point
   end
 end
