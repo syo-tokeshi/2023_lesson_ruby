9ced64ca738e7602c065aac5703f41eb494a1af6
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-11-06 08:02:55 +0900

    スコアクラスが自身のスコアを計算できるデータに変換できるようにする

--- oop_bowling/game.rb
+++ oop_bowling/game.rb
@@ -2,13 +2,12 @@ require 'frame'
 require 'score'
 class Game
   def initialize(score)
-    @score = Score.new(score) # scoreの中身は何かあまり気にせず、newすると良い感じに変換されているイメージを持つ
+    @scores = Score.new(score) # scoreの中身は何かあまり気にせず、newすると良い感じに変換されているイメージを持つ
     @frames = []
   end
 
   def total_scores
-    scores = @score.convert_score_to_scores
-    scores.each_slice(2) do |s|
+    @scores.each_slice(2) do |s|
       @frames += Frame.new(s) # ボウリングのフレームは１投目&2投目のペアなのは明確なので、この渡し方で良さそう
     end
     @frames.sum # gameが保有しているブウリングの各フレームを全部足すイメージ
--- oop_bowling/score.rb
+++ oop_bowling/score.rb
@@ -1,9 +1,18 @@
 class Score
   def initialize(score)
-    @score = score
+    @scores = convert_score_to_scores(score)
   end
 
-  def convert_score_to_scores
-    @score.split(',')
+  def convert_score_to_scores(score)
+    scores = score.split(',')
+    scores.map do |s|
+      if s == 'X'
+        [10,0].flatten
+      else
+        s.to_i
+      end
+    end
   end
+
+
 end
