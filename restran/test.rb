ae611b62de9e0497f38503961ed68cb30cf1375f
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-12-10 17:23:41 +0900

    setupで共通処理まとめる + コメントで分かりやすくする

--- restran/test.rb
+++ restran/test.rb
@@ -2,18 +2,21 @@ require_relative '' 'menu'
 require 'minitest/autorun'
 
 class MenuTest < Minitest::Test
-  def test_min_onigiri_price
+
+  def setup
     menu1 = Menu.new("onigiri", 200)
+    menu4 = Menu.new("yakiniku-teisyoku", 3000)
+  end
+
+  def test_min_onigiri_price
     assert_operator menu1.get_total_price(3), :>=, 500
   end
 
   def test_min_yakiniku_price
-    menu4 = Menu.new("yakiniku-teisyoku", 3000)
-    assert_operator menu4.get_total_price(10), :>=, 27000
+    assert_operator menu4.get_total_price(10), :>=, 27000 # これより安いと儲けが少ない
   end
 
   def test_max_yakiniku_price
-    menu4 = Menu.new("yakiniku-teisyoku", 3000)
-    assert_operator menu4.get_total_price(10), :<=, 28000
+    assert_operator menu4.get_total_price(10), :<=, 28000 # これを上回ると高いのでお得じゃない
   end
 end
