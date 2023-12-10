287d77ca9015dd88064875e634654bcb9318f39e
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-12-10 18:06:01 +0900

    setupで定義する変数を@にして、クラス内からアクセスできるように変更する

--- restran/test.rb
+++ restran/test.rb
@@ -1,22 +1,23 @@
-require_relative '' 'menu'
+# frozen_string_literal:true
+
+require_relative 'menu'
 require 'minitest/autorun'
 
 class MenuTest < Minitest::Test
-
   def setup
-    menu1 = Menu.new("onigiri", 200)
-    menu4 = Menu.new("yakiniku-teisyoku", 3000)
+    @menu1 = Menu.new("onigiri", 200)
+    @menu4 = Menu.new("yakiniku-teisyoku", 3000)
   end
 
   def test_min_onigiri_price
-    assert_operator menu1.get_total_price(3), :>=, 500
+    assert_operator @menu1.get_total_price(3), :>=, 500
   end
 
   def test_min_yakiniku_price
-    assert_operator menu4.get_total_price(10), :>=, 27000 # これより安いと儲けが少ない
+    assert_operator @menu4.get_total_price(10), :>=, 27000 # これより安いと儲けが少ない
   end
 
   def test_max_yakiniku_price
-    assert_operator menu4.get_total_price(10), :<=, 28000 # これを上回ると高いのでお得じゃない
+    assert_operator @menu4.get_total_price(10), :<=, 28000 # これを上回ると高いのでお得じゃない
   end
 end
